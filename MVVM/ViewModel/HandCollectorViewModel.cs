d5d0df2d8edd34e302893deb7ef42419670d6c00
nichlas schmidt <nichlasschmidt@hotmail.com>
2022-06-21 19:38:08 +0200

    Add project files.

---
+++ MVVM/ViewModel/HandCollectorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rake_Counter.MVVM.ViewModel
+{
+    internal class HandCollectorViewModel
+    {
+    }
+}
---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rake_Counter.Core;
+
+namespace Rake_Counter.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        // Commands
+
+        public RelayCommand RakeCounterViewCommand { get; set; }
+        public RelayCommand HandCollectorViewCommand { get; set; }
+
+
+
+        // ViewModels
+
+        public HandCollectorViewModel HandCollectorVM { get; set; }
+        public RakeCounterViewModel RakeCounterVM { get; set; }
+
+
+        // Props
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            RakeCounterVM = new RakeCounterViewModel();
+            HandCollectorVM = new HandCollectorViewModel();
+            CurrentView = RakeCounterVM;
+
+            RakeCounterViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RakeCounterVM;
+            });
+
+            HandCollectorViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HandCollectorVM;
+            });
+        }
+    }
+}
